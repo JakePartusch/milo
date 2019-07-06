33798c6856882ef387a64821630bdb87c34f73d4
Jacob Partusch <jakepartusch@gmail.com>
2019-07-06 16:20:58 -0500

    feat: query contentful for avatar

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,24 @@
 import React from "react"
 import styled from "@emotion/styled"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 
-const Avatar = styled.img({
+const Avatar = styled(Img)({
   width: 150,
   height: 150,
   borderRadius: "50%",
-  '@media(max-width:600px)': {
-    width: 100,
-    height: 100
-  }
+  "@media(max-width:600px)": {
+    width: 75,
+    height: 75,
+  },
 })
 
 const Container = styled.div({
   display: "flex",
   alignItems: "center",
   justifyContent: "center",
-  margin: "2rem 0",
+  padding: "1rem 0",
+  margin: "1rem 0",
   borderBottom: "solid thin rgb(239,239,239)",
 })
 
@@ -25,6 +28,7 @@ const Content = styled.div({
 
 const User = styled.h1({
   fontWeight: 300,
+  fontSize: "1.5rem",
   margin: 0,
 })
 
@@ -35,12 +39,35 @@ const Tagline = styled.p({
 })
 
 const Header = () => {
+  const data = useStaticQuery(graphql`
+    query UserQuery {
+      allContentfulUser {
+        edges {
+          node {
+            id
+            avatar {
+              fluid(maxHeight: 250, maxWidth: 250) {
+                src
+                srcSet
+                aspectRatio
+                sizes
+              }
+            }
+            firstName
+            lastName
+            tagLine
+          }
+        }
+      }
+    }
+  `)
+  const user = data.allContentfulUser.edges[0].node
   return (
     <Container>
-      <Avatar src="https://scontent-dfw5-2.cdninstagram.com/vp/4aa899bcd51fdf94948e126072bcf762/5D7895E7/t51.2885-19/s320x320/36147710_2138373683111596_3010327068639494144_n.jpg?_nc_ht=scontent-dfw5-2.cdninstagram.com" />
+      <Avatar fluid={user.avatar.fluid} />
       <Content>
-        <User>Milo Partusch</User>
-        <Tagline>Eat 🍼 Sleep 😴 Poop 💩</Tagline>
+        <User>{`${user.firstName} ${user.lastName}`}</User>
+        <Tagline>{user.tagLine}</Tagline>
       </Content>
     </Container>
   )
