1134dad4ac0082e077206f0e0db30a0598982ca9
Jacob Partusch <jakepartusch@gmail.com>
2019-07-06 14:33:42 -0500

    feat: enabling pwa and updating metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,9 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Milo Partusch`,
+    description: `A personal gallery for Milo`,
+    author: `@jakepartusch`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,34 +20,31 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Milo Partusch`,
+        short_name: `Milo`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#eee`,
+        theme_color: `#eee`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/mp-logo-512.png`,
       },
     },
     {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: `9ljeh5a9s52s`,
-        // Learn about environment variables: https://gatsby.dev/env-vars
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
     `gatsby-transformer-sharp`,
     `gatsby-transformer-remark`,
     `gatsby-plugin-sharp`,
-     {
+    {
       resolve: `gatsby-plugin-typography`,
-        options: {
-          pathToConfigModule: `src/utils/typography`,
-        }
-      }
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    `gatsby-plugin-offline`,
   ],
 }
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,19 +1,8 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
 const path = require(`path`)
 
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
   const blogPostTemplate = path.resolve(`src/templates/Post.js`)
-  // Query for markdown nodes to use in creating pages.
-  // You can query for whatever data you want to create pages for e.g.
-  // products, portfolio items, landing pages, etc.
-  // Variables can be added as the second function parameter
   return graphql(`
     query AllPosts {
       allContentfulPost {
@@ -48,19 +37,10 @@ exports.createPages = ({ graphql, actions }) => {
     // Create blog post pages.
     result.data.allContentfulPost.edges.forEach(edge => {
       createPage({
-        // Path for this page — required
         path: `/posts/${edge.node.id}`,
         component: blogPostTemplate,
         context: {
           post: edge.node,
-          // Add optional context data to be inserted
-          // as props into the page component..
-          //
-          // The context data can also be used as
-          // arguments to the page GraphQL query.
-          //
-          // The page "path" is always available as a GraphQL
-          // argument.
         },
       })
     })
