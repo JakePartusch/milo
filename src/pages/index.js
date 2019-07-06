c24b8c97140e3474f9737e87f304b5d3fe290827
Jacob Partusch <jakepartusch@gmail.com>
2019-07-05 23:04:59 -0500

    fix: fixing header import

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import PostGrid from "../components/PostGrid"
-import Header from "../components/Header"
+import Header from "../components/header"
 
 const IndexPage = () => (
   <Layout>
