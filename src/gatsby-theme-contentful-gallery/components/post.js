069ed5c532f0cd027fe8b79181bb15b287e12436
Jacob Partusch <jakepartusch@gmail.com>
2020-01-19 22:40:15 -0600

    fix: fixing loop on mobile

--- src/gatsby-theme-contentful-gallery/components/post.js
+++ src/gatsby-theme-contentful-gallery/components/post.js
@@ -3,13 +3,16 @@ import Post from "gatsby-theme-contentful-gallery/src/components/post"
 import { AuthService, useAuth } from "gatsby-theme-auth0"
 
 const Index = props => {
-  const { isLoggedIn } = useAuth()
+  const { isLoggedIn, isLoading } = useAuth()
 
   useEffect(() => {
+    if (isLoading) {
+      return
+    }
     if (!isLoggedIn) {
       AuthService.login()
     }
-  }, [isLoggedIn, AuthService])
+  }, [isLoggedIn, isLoading, AuthService])
 
   if (!isLoggedIn) {
     return <></>
